8b1e88cf0a3fb28b35ca2d81f48f575315e6e4c7
CL Dixon <cl_dixon@icloud.com>
2026-02-20 18:03:12 -0600

    build install fixes

--- create-igloo/bin/create-igloo.ts
+++ create-igloo/bin/create-igloo.ts
@@ -28,9 +28,9 @@ writeFileSync(
       version: "0.0.1",
       type: "module",
       scripts: {
-        dev: "bun ./node_modules/igloo-server/bin/igloo-server.ts dev",
-        build: "bun ./node_modules/igloo-server/bin/igloo-server.ts build",
-        start: "bun ./node_modules/igloo-server/bin/igloo-server.ts start",
+        dev: "igloo-server dev",
+        build: "igloo-server build",
+        start: "igloo-server start",
       },
       dependencies: {
         "igloo-server": "^0.1.0",
